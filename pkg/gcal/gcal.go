148c7b78122f9f300c3202a31a0edf354eadac7c
jvmistica <zombossjoey@tutanota.com>
2023-06-09 20:51:08 +0200

    chore: improve functions getVacationsWithoutLeaves and formatFreeTime

--- pkg/gcal/gcal.go
+++ pkg/gcal/gcal.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -45,49 +44,55 @@ type Suggestion struct {
 	End      string
 }
 
-// Query
-func Query(key string, start *string, end *string, calendarID *string) {
-	id := url.QueryEscape(*calendarID)
-	query := fmt.Sprintf("key=%s&timeMin=%s&timeMax=%s", key, *start, *end)
+// GetCalendarEvents
+func GetCalendarEvents(key, start, end, calendarID string) ([]map[string]string, []*Suggestion, error) {
+	id := url.QueryEscape(calendarID)
+	query := fmt.Sprintf("key=%s&timeMin=%s&timeMax=%s", key, start, end)
 	url := fmt.Sprintf(eventsListUrl+query, id)
 
 	var events *Events
-	var err error
-	filePath := fmt.Sprintf(defaultResultDir, fmt.Sprintf("%s.%s", *calendarID, "json"))
+	filePath := fmt.Sprintf(defaultResultDir, fmt.Sprintf("%s.%s", calendarID, "json")) // change filePath
 
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
 		fmt.Println("Initiating GET request..")
+
+		var err error
 		events, err = queryCalendarAPI(events, url, filePath)
 		if err != nil {
-			log.Fatal(err)
+			return nil, nil, err
 		}
 	} else {
 		fmt.Println("Skipping GET request..")
 
 		data, err := os.ReadFile(filePath)
 		if err != nil {
-			log.Fatal(err)
+			return nil, nil, err
 		}
 
 		if err := json.Unmarshal(data, &events); err != nil {
-			log.Fatal(err)
+			return nil, nil, err
 		}
 	}
 
 	holidays, err := getHolidays(events)
 	if err != nil {
-		log.Fatal(err)
+		return nil, nil, err
 	}
 
-	weekends, err := getWeekends(*start, *end)
+	weekends, err := getWeekends(start, end)
 	if err != nil {
-		log.Fatal(err)
+		return nil, nil, err
 	}
 
 	freeTime := formatFreeTime(holidays, weekends)
 	vacationWithoutLeaves := getVacationsWithoutLeaves(freeTime)
-	s, _ := json.MarshalIndent(vacationWithoutLeaves, "", "    ")
-	fmt.Println(string(s))
+
+	suggestions, err := getSuggestions(vacationWithoutLeaves)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return vacationWithoutLeaves, suggestions, nil
 }
 
 // getHolidays returns a map of holidays and their date
@@ -133,29 +138,26 @@ func getVacationsWithoutLeaves(freeTime []time.Time) []map[string]string {
 	days := 0
 	fromDate := freeTime[0]
 	dates := []map[string]string{}
-	for _, v := range freeTime {
+	i := 0
+
+	for i < len(freeTime) {
+		if days == 0 {
+			fromDate = freeTime[i]
+		}
+
 		days += 1
-		if days == 1 {
-			fromDate = v
-		}
-
-		for _, d := range freeTime {
-			if v.AddDate(0, 0, 1) == d {
-				days += 1
-				toDate = d
-				v = d
+		if i == len(freeTime)-1 || freeTime[i].AddDate(0, 0, 1) != freeTime[i+1] {
+			toDate = freeTime[i]
+			if days >= defaultMinDaysWithoutLeave {
+				date := make(map[string]string)
+				date["start"] = fromDate.Format(defaultTimeFormat)
+				date["end"] = toDate.Format(defaultTimeFormat)
+				date["count"] = fmt.Sprint(days)
+				dates = append(dates, date)
 			}
+			days = 0
 		}
-
-		if days >= defaultMinDaysWithoutLeave {
-			date := make(map[string]string)
-			date["start"] = fromDate.String()
-			date["end"] = toDate.String()
-			date["count"] = fmt.Sprint(days)
-			dates = append(dates, date)
-		}
-		days = 0
-
+		i += 1
 	}
 
 	return dates
@@ -189,14 +191,14 @@ func getSuggestions(pairs []map[string]string) ([]*Suggestion, error) {
 			return nil, err
 		}
 
-		leaves := nextStart.Sub(end).Hours() / 24
+		leaves := (nextStart.Sub(end).Hours() / 24) - 1
 		if leaves <= 5 {
-			vacation := (end.Sub(start).Hours() / 24) + (nextEnd.Sub(nextStart).Hours() / 24) + leaves
+			vacation := nextEnd.Sub(start).Hours() / 24
 			if vacation-leaves > 1 {
 				suggestions = append(suggestions,
 					&Suggestion{
 						Vacation: fmt.Sprint(vacation + 1),
-						Leaves:   fmt.Sprint(leaves - 1),
+						Leaves:   fmt.Sprint(leaves),
 						Start:    d["start"],
 						End:      pairs[i+1]["end"],
 					})
@@ -217,7 +219,14 @@ func formatFreeTime(holidays, weekends []time.Time) []time.Time {
 		return freeTime[i].Before(freeTime[j])
 	})
 
-	return freeTime
+	var newList []time.Time
+	for k, v := range freeTime[0 : len(freeTime)-1] {
+		if v != freeTime[k+1] {
+			newList = append(newList, v)
+		}
+	}
+
+	return newList
 }
 
 // queryCalendarAPI gets the list of holidays from the Calendar API and writes it into a JSON file
