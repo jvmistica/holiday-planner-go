26df3c6ace716cb77361def5ad90aa417d8dca8e
jvmistica <zombossjoey@tutanota.com>
2023-06-09 21:05:56 +0200

    chore: update usage in main

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/jvmistica/gcal/pkg/gcal"
@@ -11,6 +14,11 @@ import (
 var (
 	defaultCalendarID = "en.austrian#holiday@group.v.calendar.google.com"
 	key               = os.Getenv("GCP_API_KEY")
+	suggestion        = "Leave Suggestions"
+	q1                = "Jan - Mar"
+	q2                = "Apr - Jun"
+	q3                = "Jul - Sep"
+	q4                = "Oct - Dec"
 )
 
 // go run main.go -start=2023-05-01T00:00:00Z -end=2023-05-31T00:00:00Z
@@ -21,7 +29,25 @@ func main() {
 	end := flag.String("end", "", "the end date")
 	flag.Parse()
 
-	gcal.Query(key, start, end, calendarID)
+	events, suggestions, err := gcal.GetCalendarEvents(key, *start, *end, *calendarID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e, err := json.MarshalIndent(events, "", "    ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("No leaves")
+	fmt.Println(string(e))
+
+	s, err := json.MarshalIndent(suggestions, "", "    ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Suggestions")
+	fmt.Println(string(s))
 
 	// boardID, err := trello.CreateBoard("Holidays")
 	// if err != nil {
@@ -39,18 +65,21 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 
-	// q2ListID, err := trello.CreateList(boardID, q2, "3")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// // q2ListID, err := trello.CreateList(boardID, q2, "3")
+	// // if err != nil {
+	// // 	log.Fatal(err)
+	// // }
 
-	// q3ListID, err := trello.CreateList(boardID, q3, "4")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// // q3ListID, err := trello.CreateList(boardID, q3, "4")
+	// // if err != nil {
+	// // 	log.Fatal(err)
+	// // }
 
-	// q4ListID, err := trello.CreateList(boardID, q4, "5")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// // q4ListID, err := trello.CreateList(boardID, q4, "5")
+	// // if err != nil {
+	// // 	log.Fatal(err)
+	// // }
+
+	// _, _ = trello.CreateCard(q1ListID, string(e))
+	// _, _ = trello.CreateCard(suggestListID, string(s))
 }
