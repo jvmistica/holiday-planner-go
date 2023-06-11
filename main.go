eb24c4844239c39b70ce628c7d52f4ca00d7b37f
jvmistica <zombossjoey@tutanota.com>
2023-06-11 19:21:12 +0200

    feat: created boards for vacations without leaves and suggested leaves

--- main.go
+++ main.go
@@ -1,27 +1,24 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/jvmistica/gcal/pkg/gcal"
-	// "github.com/jvmistica/gcal/pkg/trello"
+	"github.com/jvmistica/gcal/pkg/trello"
 )
 
 var (
-	defaultCalendarID = "en.austrian#holiday@group.v.calendar.google.com"
-	key               = os.Getenv("GCP_API_KEY")
-	suggestion        = "Leave Suggestions"
-	q1                = "Jan - Mar"
-	q2                = "Apr - Jun"
-	q3                = "Jul - Sep"
-	q4                = "Oct - Dec"
+	defaultTimeFormat         = "2006-01-02"
+	defaultCalendarID         = "en.austrian#holiday@group.v.calendar.google.com"
+	key                       = os.Getenv("GCP_API_KEY")
+	listSuggestions           = "Leave Suggestions"
+	listVacationWithoutLeaves = "Vacation without leaves"
 )
 
-// go run main.go -start=2023-05-01T00:00:00Z -end=2023-05-31T00:00:00Z
+// go run main.go -start=2023-05-01 -end=2023-05-31
 func main() {
 	// Parse command-line arguments
 	calendarID := flag.String("calendarId", defaultCalendarID, "the calendarID")
@@ -29,57 +26,37 @@ func main() {
 	end := flag.String("end", "", "the end date")
 	flag.Parse()
 
-	events, suggestions, err := gcal.GetCalendarEvents(key, *start, *end, *calendarID)
+	vacationWithoutLeaves, suggestions, err := gcal.GetCalendarEvents(key, *start, *end, *calendarID)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	e, err := json.MarshalIndent(events, "", "    ")
+	boardID, err := trello.CreateBoard("Holidays")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("No leaves")
-	fmt.Println(string(e))
-
-	s, err := json.MarshalIndent(suggestions, "", "    ")
+	vacationListID, err := trello.CreateList(boardID, listVacationWithoutLeaves, "1")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Suggestions")
-	fmt.Println(string(s))
 
-	// boardID, err := trello.CreateBoard("Holidays")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println("Board ID:", boardID)
+	suggestionListID, err := trello.CreateList(boardID, listSuggestions, "2")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// suggestListID, err := trello.CreateList(boardID, suggestion, "1")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	for _, i := range vacationWithoutLeaves {
+		name := fmt.Sprintf("%s - %s -> %d days", i.Start.Format(defaultTimeFormat), i.End.Format(defaultTimeFormat), i.Count)
+		if _, err := trello.CreateCard(vacationListID, name); err != nil {
+			log.Fatal(err)
+		}
+	}
 
-	// q1ListID, err := trello.CreateList(boardID, q1, "2")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// // q2ListID, err := trello.CreateList(boardID, q2, "3")
-	// // if err != nil {
-	// // 	log.Fatal(err)
-	// // }
-
-	// // q3ListID, err := trello.CreateList(boardID, q3, "4")
-	// // if err != nil {
-	// // 	log.Fatal(err)
-	// // }
-
-	// // q4ListID, err := trello.CreateList(boardID, q4, "5")
-	// // if err != nil {
-	// // 	log.Fatal(err)
-	// // }
-
-	// _, _ = trello.CreateCard(q1ListID, string(e))
-	// _, _ = trello.CreateCard(suggestListID, string(s))
+	for _, i := range suggestions {
+		name := fmt.Sprintf("%s - %s -> %d leaves / %d days", i.Start.Format(defaultTimeFormat), i.End.Format(defaultTimeFormat), i.Leaves, i.Vacation)
+		if _, err := trello.CreateCard(suggestionListID, name); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
